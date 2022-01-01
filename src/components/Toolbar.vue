c141e6e957ccf8d86d18b8a52fcb55bce33d3a78
Jerud-Moyer <jerudmoyer@gmail.com>
2021-12-31 18:37:38 -0800

    initial commit

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-toolbar>artists tools</q-toolbar>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+<Toolbar />
+  <div class='wut'>{{message}}</div>
+  <button @click="changeMessage('hey!')">Change it!</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Toolbar from 'components/Toolbar.vue';
+export default defineComponent({
+ components: {
+   Toolbar
+ },
+ data() {
+   return {
+      message: 'wazzup?'
+   };
+  },
+  methods: {
+    changeMessage(message: string) {
+      this.message = message
+    }
+  }
+});
+</script>
+
+<style>
+
+</style>
