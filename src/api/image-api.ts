3daa4cba8cbe48cb89013ffc5effe3c61bce1b41
Jerud-Moyer <jerudmoyer@gmail.com>
2022-02-26 15:01:20 -0800

    add log to api error

--- src/api/image-api.ts
+++ src/api/image-api.ts
@@ -1,12 +1,13 @@
 import { api } from 'boot/axios';
 import { ImageItem } from 'src/types/types';
+import process from 'process';
 
 const getImages = async(): Promise<ImageItem[]>=> {
   try {
     const res = await api.get('/art-works');
     return res.data;
   } catch {
-    console.log('ERRRRRRRRROR!')
+    console.log('ERRRRRRRRROR!', process.env.API_URL)
   } return []
 }
 
