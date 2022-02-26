029d32f6b76dce495dfcbbc513fc6d555b923b11
Jerud-Moyer <jerudmoyer@gmail.com>
2022-02-26 14:14:04 -0800

    add dotenv

--- quasar.conf.js
+++ quasar.conf.js
@@ -55,7 +55,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-
+      env: require('dotenv').config().parsed,
       transpile: true,
       // publicPath: '/',
 
