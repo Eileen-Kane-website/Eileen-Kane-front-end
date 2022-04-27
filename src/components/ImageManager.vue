34c5c5fb265aafe24ae26febbc9533e24b51a801
Jerud-Moyer <jerudmoyer@gmail.com>
2022-04-27 08:35:04 -0700

    a start on cloudinary

---
+++ src/components/ImageManager.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class='q-pa-lg bg-info'>
+    <div class='q-pa-lg q-mt-lg flex items-center'>
+      <div class='text-h5 text-dark q-mr-xl'>
+        add new content
+      </div>
+      <q-btn 
+        label='add image'
+        color='accent'
+        glossy
+        class='q-ma-md'
+      />
+      <q-btn 
+        label='add series'
+        color='accent'
+        glossy
+        class='q-ma-md'
+      />
+    </div>
+    <q-form class='q-pa-lg upload-form'>
+      <q-input 
+        type='file'
+        outlined
+        class='form-element'
+        @change='handleFileInput'
+      />
+      <q-input
+
+      />
+    </q-form>
+  </div>
+</template>
+
+<script lang='ts'>
+import {
+  defineComponent,
+  ref
+} from 'vue';
+
+export default defineComponent ({
+    setup () {
+      const previewSource = ref<string | null>('');
+
+      const previewFile = (file: Blob) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = (): void => {
+          if (typeof reader.result === 'string') {
+            previewSource.value = reader.result
+          }
+        }
+      }
+
+      const handleFileInput = ((e: Event): void => {
+        e.preventDefault()
+        const target = e
+          ? e.target as HTMLInputElement
+          : null
+        if (target) {
+          previewFile(target.files[0] as string)
+        } else {
+          console.log('no file selected!')
+        }
+      })  
+      
+      return {
+        previewSource,
+        handleFileInput
+      }
+  }
+
+})
+</script>
+
+<style lang='scss'>
+  .upload-form {
+    max-width: 50%;
+  }
+  .form-element {
+    width: 50%;
+  }
+
+</style>
--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,161 +1,50 @@
 <template>
   <div>
     <q-toolbar class='bg-info text-dark shadow-2 top-bar'>
-      <q-btn 
-        flat
-        label='homepage'
-        @click='goHome'
-        class='q-ml-lg'
-      />
+          <q-btn
+            @click='goHome'
+            flat
+            label='homepage'
+            class='q-ml-lg'
+          />
     </q-toolbar>
-    <div class='bg-info q-pa-lg row admin-page'>
-      <div class='q-pa-md flex row image-list'>
-        <q-item
-          v-for='image in images' 
-          :key='image.title'
-          tag='label' 
-          :class="[(previewMode) ? 'low-opacity' : '', 'q-ma-sm', 'column', 'list-item']"
-        >
-          <q-checkbox
-            v-model='featured' 
-            :disable='previewMode'
-            :val='image.title' 
-            color='accent' 
-            class='check-box'
-          />
-          <q-img :src="`${image.slug}.jpeg`" class='list-image'/>
-          <div class='text-subtitle2 text-dark'>{{ image.title }}</div>
-        </q-item>
-      </div>
-      <div class='column items-center page-right'>
-        <div v-if='!previewMode' class='text-h6 q-mt-lg text-center text-dark '>
-          select images from the list, then click the button to see a preview.
-        </div>
-        <div v-if='!previewMode' class='text-subtitle1 text-dark'>
-          please note that currently featured images are already checked.
-        </div>
-        <div v-if='previewMode' class='text-h6 q-mt-lg text-center text-dark' style='max-width: 80%'>
-          This is a preview only. To commit these changes you must click the submit button below.
-        </div>
-        <div class='row q-ma-lg'>
-          <q-btn
-            v-if="!previewMode"
-            color="dark"
-            glossy
-            label='preview'
-            class='q-ma-md'
-            @click='handlePreview'
-          />
-        </div>
-        <ImageCarousel
-          v-if='previewMode'
-          :images='previewImages'
-          :model='slide'
-        />
-        <div class='row q-ma-lg'>
-          <q-btn
-            v-if='previewMode'
-            color="secondary"
-            glossy
-            label='cancel'
-            class='q-ma-md'
-            @click='cancelPreview'
-          />
-          <q-btn
-            v-if='previewMode'
-            color="dark"
-            glossy
-            label='submit'
-            class='q-ma-md'
-            @click='handleSubmit'
-          />
-        </div>
-      </div>
-    </div>
+    <ImageManager />
+    <FeaturedImageManager />
   </div>
 </template>
 
 <script lang='ts'>
-  /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import {
-  defineComponent,
+import { 
+  defineComponent, 
   onBeforeMount,
-  ComputedRef,
-  computed,
-  Ref,
-  ref
+  onMounted,
+  computed 
 } from 'vue';
-import ImageCarousel from 'src/components/Image-Carousel.vue';
-import { useRouter } from 'vue-router';
-import authApi from 'src/api/auth-api';
+import FeaturedImageManager from 'src/components/FeaturedImageManager.vue';
+import ImageManager from 'src/components/ImageManager.vue';
 import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
 import { useStore } from 'src/store';
+import authApi from 'src/api/auth-api';
 import imageApi from 'src/api/image-api';
-import { ImageItem, FeatureUpdateItem } from 'src/types/types';
-import { AxiosResponse } from 'axios';
 
 export default defineComponent ({
   components: {
-    ImageCarousel
+    ImageManager,
+    FeaturedImageManager
   },
   setup() {
-    const $q = useQuasar();
     const router = useRouter();
+    const $q = useQuasar();
     const store = useStore();
-    const slide = ref<string>('');
-    const previewMode = ref<boolean>(false);
-    const images: ComputedRef<ImageItem[]> = computed(() => store.getters['portfolio/getAllImages']);
-    const userName: ComputedRef<string> = computed(() => store.state.user.firstName)
-    const token: ComputedRef<string> = computed(() => store.state.user.token);
-    const currentFeatured: ComputedRef<ImageItem[]> = computed(() => store.getters['portfolio/getFeaturedImages']);
-    const featuredNames: ComputedRef<string[]> = computed(() => (
-      currentFeatured.value.map((image: ImageItem) => image.title)
-    ))
-    const unFeatured = computed(() => featuredNames.value.filter(name => (
-      !featured.value.includes(name)
-    )))
-    const featured: Ref<string[]> = ref(featuredNames.value);
 
-    const previewImages: ComputedRef<ImageItem[]> = computed(() => store.getters['portfolio/getFeaturePreview'](featured.value))
-
-    const newlyFeatured = computed(() => featured.value.filter(image => (!featuredNames.value.includes(image))));
-    
-    const featuredIds: ComputedRef<FeatureUpdateItem[]> = computed(() => 
-      store.getters['portfolio/getImageIdsByTitle']
-        (newlyFeatured.value, true));
-
-    const unFeaturedIds: ComputedRef<FeatureUpdateItem[]> = computed(() =>
-      store.getters['portfolio/getImageIdsByTitle']
-        (unFeatured.value, false));
-
-    const handleNotify = (res: AxiosResponse | undefined) => {
-      (res && res.status === 200)
-        ? void $q.notify('Your featured images have been updated!')
-        : void $q.notify('Sorry, something went wrong.')
-    }
+    const token = computed<string>(() =>
+      store.state.user.token);
+    const userName = computed<string>(() => 
+      store.state.user.firstName)
 
     const goHome = () => {
       void router.push('/')
-    }
-
-    const handlePreview = () => {
-      previewMode.value = true;
-    }
-
-    const cancelPreview = () => {
-      previewMode.value = false;
-    }
-
-    const handleSubmit = () => {
-      void imageApi.updateFeaturedImages([
-        ...featuredIds.value,
-        ...unFeaturedIds.value
-      ])
-        .then(res => handleNotify(res))
-      void imageApi.getImages()
-        .then((images: ImageItem[]) => store.dispatch('portfolio/setImages', images))
-      previewMode.value = false
     }
 
     onBeforeMount(() => {
@@ -163,24 +52,17 @@ export default defineComponent ({
         .then(res => res.ok
           ? void $q.notify(`Hello ${userName.value}!`)
           : router.push('/login')
-        )
+        ) 
+    })
+
+    onMounted(() => {
       void imageApi.getImages()
         .then((fetchedImages: ImageItem[]) => (
           store.dispatch('portfolio/setImages', fetchedImages)
         ))
-      slide.value = currentFeatured.value[0].title 
-    })
+    }) 
 
     return {
-      images,
-      featured,
-      currentFeatured,
-      previewImages,
-      previewMode,
-      slide,
-      handlePreview,
-      cancelPreview,
-      handleSubmit,
       goHome
     }
   }
@@ -193,35 +75,5 @@ export default defineComponent ({
     z-index: 2;
     position: fixed;
   }
-  .admin-page{
-    min-height: 100vh;
-    padding-top: 100px;
-    justify-content: space-around;
-  }
-  .image-list {
-    height: 80%;
-    width: 50%;
-    border: solid $dark 1px;
-    border-radius: 5px;
-  }
-  .list-item {
-    position: relative;
-  }
-  .list-image {
-    width: 200px;
-    max-height: 250px;
-    object-fit: cover;
-  }
-  .check-box {
-    position: absolute;
-    top: 5px;
-    left: 15px;
-    z-index: 1;
-  }
-  .page-right {
-    width: 40%;
-  }
-  .low-opacity {
-    opacity: .5;
-  }
+
 </style>
