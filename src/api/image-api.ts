ad3af88975605ced834b105198e74f5f98f465f8
Jerud-Moyer <jerudmoyer@gmail.com>
2022-06-08 23:15:15 -0700

    try fetch instead of Axios for image upload

--- src/api/image-api.ts
+++ src/api/image-api.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+
 import { api } from 'boot/axios';
 import { 
     ImageItem, 
@@ -26,22 +29,38 @@ const updateFeaturedImages = async(featureUpdateImages: FeatureUpdateItem[]) => 
   } 
 }
 
+// const uploadImage = async(imageBlob: unknown, imageData: NewImage):
+//   Promise<ImageItem | string> => {
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const res = await api.post('art-works', {data: {
+//       imageBlob,
+//       imageData
+//     },
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Access-Control-Allow-Origin': '*'
+//     }
+//   })
+//     if(res.status !== 200) return res.statusText
+//     console.log('api-res => ', res)
+//     return res.data
+// }
+
 const uploadImage = async(imageBlob: unknown, imageData: NewImage):
   Promise<ImageItem | string> => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const res = await api.post('art-works', {data: {
-      imageBlob,
-      imageData
-    },
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
-    if(res.status !== 200) return res.statusText
-    console.log('api-res => ', res)
-    return res.data
-}
+    const res = await fetch(`${process.env.API_URL}/art-works`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include',
+      body: JSON.stringify({
+        imageBlob,
+        imageData
+      })
+    })
+    const json = await res.json();
+  if(!res.ok) throw json;
+  return json;
+  }
 
 const deleteImage = async(images: DeleteImageItem[]) => {
   try {
